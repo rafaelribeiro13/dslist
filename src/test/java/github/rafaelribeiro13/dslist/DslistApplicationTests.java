6e4fdfc58d903b8fcc07a7290c6fd3698b238a0f
Rafael Ribeiro <rafaelsribeiro45@gmail.com>
2023-05-08 22:06:31 -0300

    first commit

---
+++ src/test/java/github/rafaelribeiro13/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package github.rafaelribeiro13.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
